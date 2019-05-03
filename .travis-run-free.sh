9dc7c2879629c26543ce27ec0a47e6bbe13eb7a5
Jacob Goense <dugo@xs4all.nl>
2019-05-03 11:45:19 +0200

    push

--- .travis-run-free.sh
+++ .travis-run-free.sh
@@ -1,3 +1,11 @@
 set -e
 make build/libv86.js
 ls -ltr ` find ./ -type f`
+rm -rf ./.git
+GHP_URL=https://${GHP_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+git init
+git config user.name "${USER}"
+git config user.email "${GHP_MAIL}"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "${GHP_URL}" master:gh-pages
