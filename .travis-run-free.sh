89708d4604adb9fcfcbec19a626b9bd446869b1d
Jacob Goense <dugo@xs4all.nl>
2019-05-03 12:44:10 +0200

    Update .travis-run-free.sh

--- .travis-run-free.sh
+++ .travis-run-free.sh
@@ -3,12 +3,11 @@ make build/libv86.js
 make build/v86_all.js
 #cp -p ./build/* ./
 ls -ltr ` find ./ -type f`
-rm -rf ./.git
+rm -rf ./.git ./.gitignore
 GHP_URL=https://${GHP_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 git init
 git config user.name "${USER}"
 git config user.email "${GHP_MAIL}"
-git add .
-git add build
+git add *
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "${GHP_URL}" master:gh-pages
