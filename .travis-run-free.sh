4917d53b366a9862892a422eff98252095a32960
Jacob Goense <dugo@xs4all.nl>
2019-05-06 17:37:09 +0200

    Update .travis-run-free.sh

--- .travis-run-free.sh
+++ .travis-run-free.sh
@@ -1,9 +1,10 @@
 set -e
 make build/libv86.js
 make build/v86_all.js
-#cp -p ./build/* ./
 ls -ltr ` find ./ -type f`
 rm -rf ./.git ./.gitignore
+mkdir -p images
+(cd images && curl --compressed -O https://copy.sh/v86/images/linux3.iso)
 GHP_URL=https://${GHP_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 git init
 git config user.name "${USER}"
